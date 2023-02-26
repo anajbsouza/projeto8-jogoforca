bf91695d2b5d67ad2e097791b354bdf049a0cced
anajbsouza <anaju.batsouza@gmail.com>
2023-02-26 17:29:10 -0300

    primeiro commit

---
+++ src/Components/Iniciar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Iniciar() {
+    return (
+        <div>Iniciar</div>
+    )
+}
---
+++ src/Components/Jogo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Imagem from "./Imagem";
+import Iniciar from "./Iniciar";
+import Letras from "./Letras";
+
+export default function Jogo() {
+    return (
+        <>
+        <Imagem />
+        <Iniciar />
+        <Letras />
+        </>
+    );
+    }
